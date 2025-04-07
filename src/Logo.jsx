863ae3c5d2002c376ac952fe8dd524f3c543b330
shankar <annoytrex@gmail.com>
2025-04-07 22:18:25 +0300

    update 10

--- src/Logo.jsx
+++ src/Logo.jsx
@@ -9,7 +9,7 @@ const Logo = () => {
     <div className="flex items-center">
       <div className="relative w-12 h-12 rotate-12 flex items-center justify-center pr-3">
         <img
-          src="/images/Vector.svg"
+          src="public/images/Vector.svg"
           alt="Logo"
           className="w-10 h-10 cursor-pointer hover:opacity-80 transition rounded-lg"
           onClick={handleRefresh}
